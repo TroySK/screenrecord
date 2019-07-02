c5d8ab291d31bfc3939fdc9dbadecaa8f1b70696
Troy SK <shirsendu.karmakar@gmail.com>
2019-07-02 17:54:04 +0530

    Updates

---
+++ precache-manifest.26a21d7d8df8e6dfd3694d3c2b7971a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8593bc168cdc8906d02f8b2efe4ca655",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e84ef7866dd3379ee2",
+    "url": "/static/css/main.47da2750.chunk.css"
+  },
+  {
+    "revision": "6ceae9b5d4767324e0e3",
+    "url": "/static/js/2.a6d1e61f.chunk.js"
+  },
+  {
+    "revision": "59e84ef7866dd3379ee2",
+    "url": "/static/js/main.6c6884fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
